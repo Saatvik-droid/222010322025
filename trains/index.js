58e4f1d848b27961213d190073a090395cd6b724
Saatvik Ramani <saatvikramani777@gmail.com>
2023-08-09 12:07:51 +0530

    add

--- trains/index.js
+++ trains/index.js
@@ -6,12 +6,57 @@ let token =
 let headers = {
   Authorization: `Bearer ${token}`,
 };
+
+const handle = (data) => {
+  let res = [];
+  data.forEach((train) => {
+    if (
+      train.departureTime.Hours > 1 ||
+      train.departureTime.Minutes > 30 ||
+      train.departureTime.Minutes + train.delayedBy > 30 ||
+      (train.departureTime.Minutes == 30 && train.departureTime.Seconds > 0)
+    ) {
+      res.push(train);
+    }
+  });
+  res.sort((a, b) => {
+    if (a.price.sleeper != b.price.sleeper) {
+      return a.price.sleeper - b.price.sleeper;
+    }
+    if (a.price.AC != b.price.AC) {
+      return a.price.AC - b.price.AC;
+    }
+    if (a.seatsAvailable.sleeper != b.seatsAvailable.sleeper) {
+      return b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
+    }
+    if (a.seatsAvailable.AC != b.seatsAvailable.AC) {
+      return b.seatsAvailable.AC - a.seatsAvailable.AC;
+    }
+    if (a.departureTime.Hours != b.departureTime.Hours) {
+      return b.departureTime.Hours - a.departureTime.Hours;
+    }
+    if (
+      a.departureTime.Minutes + a.delayedBy !=
+      b.departureTime.Minutes + b.delayedBy
+    ) {
+      return (
+        b.departureTime.Minutes +
+        b.delayedBy -
+        a.departureTime.Minutes +
+        a.delayedBy
+      );
+    }
+    if (a.departureTime.Seconds != b.departureTime.Seconds) {
+      return b.departureTime.Seconds - a.departureTime.Seconds;
+    }
+  });
+  return res;
+};
 app.get("/trains", async (req, res) => {
-  console.log(headers);
+  let s = "";
   let val = await fetch("http://20.244.56.144/train/trains", { headers });
   val.json().then(async (r) => {
     if (r["message"].includes("token is expired")) {
-      console.log("new token");
       const postData = {
         companyName: "Train Central",
         clientID: "569422d4-9d67-4b69-9bda-738963beadd8",
@@ -25,22 +70,24 @@ app.get("/trains", async (req, res) => {
         body: JSON.stringify(postData),
       };
       val = await fetch("http://20.244.56.144/train/auth", requestOptions);
-      val.json().then(
-        (r) =>
-          (headers = {
-            Authorization: `Bearer ${token}`,
-          })
-      );
-      val = await fetch("http://20.244.56.144/train/trains", { headers });
-      val.json().then((r) => console.log(r));
+      val.json().then(async (r) => {
+        headers = {
+          Authorization: `Bearer ${r["access_token"]}`,
+        };
+        val = await fetch("http://20.244.56.144/train/trains", { headers });
+        val.json().then((r) => {
+          s = handle(r);
+          res.json(s);
+        });
+      });
     } else {
-      console.log(r);
+      s = handle(r);
+      res.json(s);
     }
   });
-  res.sendStatus(200);
 });
 
-app.listen(5000, () => {
-  console.log(`Started on http://localhost:5000`);
+app.listen(5001, () => {
+  console.log(`Started on http://localhost:5001`);
 });
 
